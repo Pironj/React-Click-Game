66436bf09f81f6f1b57c03b139e6480200f707f3
Julian Piron <pironj27@gmail.com>
2019-07-10 23:12:04 -0700

    Updates

---
+++ precache-manifest.d662123bd982bf46c516dac3f14693b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae24b7cc6c91aefa30f9a1349ab0058",
+    "url": "/React-Click-Game/index.html"
+  },
+  {
+    "revision": "a1a0db19a57163137f66",
+    "url": "/React-Click-Game/static/css/main.d93c08cc.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/React-Click-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a1a0db19a57163137f66",
+    "url": "/React-Click-Game/static/js/main.72f24025.chunk.js"
+  },
+  {
+    "revision": "9ec4adcd389b3cafea1c",
+    "url": "/React-Click-Game/static/js/runtime~main.2be38ff2.js"
+  }
+]);
